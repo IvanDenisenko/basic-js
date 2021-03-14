8b02c58951596efac5176d4655bf7237220fcd50
Ivan Denisenko <57327265+IvanDenisenko@users.noreply.github.com>
2021-03-14 17:58:58 +0300

    init: new createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
+  let alph = [];
+if (members == null || members == false || members == undefined ) return false;
+   for (let i = 0; i < members.length; i++ ){
+  if (typeof(members[i]) == "string"){
+  alph += members[i].charAt(0);
+  }
+else i++;
+  }
+  a = alph.split("").sort().join("");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  return a.toUpperCase();
+   
+  };
