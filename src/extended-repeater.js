fe98d378743ceb8cedc2ee42036f5649f4bba4ff
Ivan Denisenko <57327265+IvanDenisenko@users.noreply.github.com>
2021-03-15 20:41:01 +0300

    init: new repeater decision

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let repeatTimes  = options.repeatTimes;
+  let separator = options.separator || '+';
+  let addition = options.addition !== undefined ? '' + options.addition : '';
+  let additionStr = '';
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let additionSeparator = options.additionSeparator || '';
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+if  (repeatTimes > 0 ){ 
+  (str + additionStr + separator).repeat(repeatTimes - 1) + str + additionStr
+}
+ else str + additionStr;
+  return repeatTimes
 };
   
